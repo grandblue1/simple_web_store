41eddc0f5e6dc3e7932eda2485ffe3eb856912d6
grandblue1 <109659492+grandblue1@users.noreply.github.com>
2023-09-20 16:44:12 +0300

    Update db.js

--- server/bd/db.js
+++ server/bd/db.js
@@ -1,7 +1,13 @@
 const Pool = require('pg').Pool;
 
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-})
+const pool = new Pool(
+    {
+        user: process.env.POSTGRES_USER || "postgres",
+        password: process.env.POSTGRES_PASSWORD || "admin",
+        host: process.env.POSTGRES_HOST ||"localhost",
+        port: process.env.POSTGRES_PORT || "5432",
+        database: process.env.POSTGRES_DB || "sql"
+    }
+);
 
 module.exports = pool;
